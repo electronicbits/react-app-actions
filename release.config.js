cea9ee261abde99fdcdbea5c46ac7a5a02119f29
unknown <dario@electronic-bits.com>
2021-02-08 17:03:28 +1100

    fix format

--- release.config.js
+++ release.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    branches: "main",
-    respositoryUrl: "https://github.com/electronicbits/react-app-actions",
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
-        '@semantic-release/github'
-    ]
-}
+  branches: "main",
+  respositoryUrl: "https://github.com/electronicbits/react-app-actions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
